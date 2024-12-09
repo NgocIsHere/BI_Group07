4f1d75b08d1e61ac6f2f5ca9c21e4858e9ebafea
Hoa-Vien Vo-Hoang <115332302+hoavien0110@users.noreply.github.com>
2024-12-09 20:58:26 +0700

    Update QueryDDS.sql

--- CQ.BI.2425.07_ETL/QueryDDS.sql
+++ CQ.BI.2425.07_ETL/QueryDDS.sql
@@ -4,4 +4,7 @@ select * from Dim_County
 select * from Fact_AQI
 
 use BI_THUCHANH_1
-select * from NDS_AQI
+select * from NDS_State
+select * from NDS_County
+select * from NDS_Time
+SELECT * FROM NDS_AQI
